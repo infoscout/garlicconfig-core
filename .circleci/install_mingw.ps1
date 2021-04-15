56a9b48a58f8cd053e973c6c791f9695ce7ba890
Peyman <pey.mortazavi@gmail.com>
2021-04-15 07:54:53 -1000

    remove circleci entirely

--- .circleci/install_mingw.ps1
+++
@@ -1,9 +0,0 @@
-if (Test-Path C:\mingw\bin) { "MinGW Already Exists." }
-else {
-  $client = New-Object System.Net.WebClient
-  $client.DownloadFile("https://osdn.net/frs/redir.php?m=constant&f=mingw%2F68260%2Fmingw-get-0.6.3-mingw32-pre-20170905-1-bin.zip", "C:\mingw.zip")
-  mkdir C:\mingw
-  unzip C:\mingw.zip -d C:\mingw
-  cd C:\mingw\bin\
-  .\mingw-get.exe install gcc g++ mingw32-make
-}
